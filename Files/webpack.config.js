85ed217e7a8590eaa7527a5d4543fdb6d07330ae
Roy Barel <rbarel@golantelecom.co.il>
2025-01-14 12:18:45 +0200

    poc/CLI-To-Webpack4

--- Files/webpack.config.js
+++ Files/webpack.config.js
@@ -1,19 +1,10 @@
-const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   mode: 'development', // or 'production'
   entry: './src/main.ts',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  resolve: {
-    extensions: ['.ts', '.js']
-  },
   module: {
     rules: [
       {
@@ -21,37 +12,43 @@ module.exports = {
         use: [
           {
             loader: 'ts-loader',
-            options: { configFile: 'tsconfig.app.json' }
+            options: {configFile: 'tsconfig.json'},
           },
-          'angular2-template-loader'
+          'angular2-template-loader',
         ],
-        exclude: /node_modules/
       },
       {
         test: /\.html$/,
-        use: 'html-loader'
+        use: 'raw-loader',
       },
       {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
-      }
-    ]
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    alias: {
+      '@app': path.resolve(__dirname, 'src/app'),
+      '@environments': path.resolve(__dirname, 'src/environments'),
+    },
   },
   plugins: [
-    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: './src/index.html',
+      filename: 'index.html',
+      inject: 'body'
     }),
-    new MiniCssExtractPlugin()
   ],
-  optimization: {
-    minimizer: [new TerserPlugin()]
-  },
   devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist')
-    },
     compress: true,
-    port: 4200
-  }
+    port: 4200,
+    historyApiFallback: true,
+  },
 };
--- Install.sh
+++ Install.sh
@@ -1,56 +1,96 @@
 #!/usr/bin/env bash
 
-#rm -vfr node_modules
+#echo "BASH_SOURCE: ${BASH_SOURCE[0]}"
 
-npm uninstall @angular/cli
+if [ -z "${ORIGIN_DIR}" ]; then
+	ORIGIN_DIR=$(pwd)
+	BASH_SOURCE_ORIGIN="${BASH_SOURCE[0]}"
+fi
 
-#npm install --save-dev webpack webpack-cli webpack-dev-server html-webpack-plugin clean-webpack-plugin mini-css-extract-plugin terser-webpack-plugin ts-loader angular2-template-loader html-loader css-loader style-loader
+if [ -z "${PROJECT_ROOT}" ]; then
+	PROJECT_ROOT=$(dirname $(realpath "${BASH_SOURCE_ORIGIN}"))
+	cd "${PROJECT_ROOT}" || exit 1
+	PROJECT_ROOT=$(pwd)
+fi
 
-npm uninstall \
-codelyzer \
-jasmine-core \
-jasmine-spec-reporter \
-karma \
-karma-chrome-launcher \
-karma-cli \
-karma-coverage-istanbul-reporter \
-karma-jasmine \
-karma-jasmine-html-reporter \
-karma-junit-reporter \
-karma-spec-reporter \
-karma-webpack \
-protractor
-#ts-node \
-#tslint
+if [ "$(declare -p FUNCTIONS_INCLUDE 2>/dev/null)"  == '' ] || [ -z "${FUNCTIONS_INCLUDE[$(pwd)"/bashColours.sh"]}" ]; then
+	. "${PROJECT_ROOT}/CLI/lib/bashColours.sh"
+fi
 
-#npm uninstall \
-#@angular-devkit/build-angular \
-#@angular/compiler-cli \
-#@angular/language-service \
-#@types/jasmine \
-#@types/jasminewd2 \
-#@types/node
+case "$*" in
+	*--verbose* | *-v*)
+		info "Verbose"
+		VERBOSE=1
+		;;
+	*--dry-run*)
+		info "Dry run"
+		DRY_RUN=1
+		;;
+	*--clear-cache*)
+		rm -vfr node_modules
+		npm cache clean --force
+		;;
+	*--clear-modules*)
+		rm -vfr node_modules
+		;;
+	*--install*)
+		INSTALL='true'
+		;;
+	*) # unrecognized option - show help or ignore
+		# warning "HOSTNAME not matched ()"
+		;;
+esac
 
-#npm uninstall \
-#@angular/animations \
-#@angular/http
+UNINSTALL=''
+UNINSTALL+=' @angular/cli'
 
-npm install --save-dev \
-@types/node@^10.12.0 \
-@types/node@^10.12.0 \
-angular2-template-loader@^0.6.2 \
-codelyzer@~4.5.0
-html-webpack-plugin@^3.2.0 \
-protractor@~5.4.0
-raw-loader@^0.5.1 \
-ts-loader@^5.2.2 \
-typescript@^3.1.3 \
-webpack-cli@^3.1.2 \
-webpack-dev-server@^3.1.10
-webpack@^4.23.1 \
-''
-npm install html-webpack-plugin@4.5.2 --save-dev
-npm install html-webpack-plugin@^3.2.0 --save-dev
+if [ -z "${UNINSTALL}" ]; then
+	npm uninstall ${UNINSTALL}
+fi
 
-npm i
-#npm run build
+DEPENDENCIES=''
+DEPENDENCIES+=' @angular/compiler@7.0.4'
+
+DEV_DEPENDENCIES=''
+DEV_DEPENDENCIES+=' angular2-template-loader@^0.6.2'
+DEV_DEPENDENCIES+=' codelyzer@~4.5.0'
+DEV_DEPENDENCIES+=' html-webpack-plugin@^3.2.0'
+DEV_DEPENDENCIES+=' protractor@~5.4.0'
+DEV_DEPENDENCIES+=' raw-loader@^0.5.1'
+DEV_DEPENDENCIES+=' ts-loader@^5.2.2'
+DEV_DEPENDENCIES+=' webpack-cli@^3.1.2'
+DEV_DEPENDENCIES+=' webpack-dev-server@^3.1.10'
+DEV_DEPENDENCIES+=' webpack@^4.23.1'
+DEV_DEPENDENCIES+=' @angular/compiler@7.2.16'
+DEV_DEPENDENCIES+=' npm install --save-dev'
+DEV_DEPENDENCIES+=' angular2-template-loader@^0.6.2'
+
+if [ -n "${DEPENDENCIES}" ]; then
+	INSTALL_COMMAND="npm install --save ${DEPENDENCIES}"
+	if [[ ${VERBOSE,,} =~ true|yes|1 ]] || [[ ${DRY_RUN,,} =~ true|yes|1 ]]; then
+		codeOutput "${INSTALL_COMMAND}"
+	fi
+	if [[ ! ${DRY_RUN,,} =~ yes|y|true|1 ]]; then
+		${INSTALL_COMMAND}
+	fi
+fi
+
+if [ -n "${DEV_DEPENDENCIES}" ]; then
+	INSTALL_DEV_COMMAND="npm install --save-dev ${DEV_DEPENDENCIES}"
+	if [[ ${VERBOSE,,} =~ true|yes|1 ]] || [[ ${DRY_RUN,,} =~ true|yes|1 ]]; then
+		codeOutput "${INSTALL_DEV_COMMAND}"
+	fi
+	if [[ ! ${DRY_RUN,,} =~ yes|y|true|1 ]]; then
+		${INSTALL_DEV_COMMAND}
+	fi
+fi
+
+if [[ ${CHECK,,} =~ true|yes|1 ]]; then
+	npm ls
+fi
+if [[ ${INSTALL,,} =~ true|yes|1 ]]; then
+	npm i
+fi
+if [[ ${START,,} =~ true|yes|1 ]]; then
+	npm start
+fi
