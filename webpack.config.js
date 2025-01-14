9a1368a14282c92c323fc06208d3890032b89f54
Roy Barel <rbarel@golantelecom.co.il>
2025-01-14 15:11:00 +0200

    poc/CLI-To-Webpack4

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    alias: {
+      '@app': path.resolve(__dirname, 'src/app'),
+      '@environments': path.resolve(__dirname, 'src/environments'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: { configFile: 'tsconfig.json' },
+          },
+          'angular2-template-loader',
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: 'raw-loader',
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  devServer: {
+    compress: true,
+    port: 4200,
+    historyApiFallback: true,
+  },
+};
